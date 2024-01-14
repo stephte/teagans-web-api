1bc338c58fd88bc43b4b10f0ecd0b3a94e485cdc
stephte <teaganstephenson@gmail.com>
2024-01-14 11:04:34 -0600

    update CORS handling

--- config/routes.go
+++ config/routes.go
@@ -32,10 +32,8 @@ func(this *Router) defineRoutes() {
 
 	this.logger.Debug().Msg("Setting up routes")
 
-	// allow CORS in dev enviroment
-	if this.env == "dev" {
-		r.Use(middle.AllowLocalHost)
-	}
+	// uses CHI_YT_WEBAPP_ORIGIN to allow requests from given origin
+	r.Use(middle.AllowCORS)
 
 	r.Get("/", controllers.Index)
 
