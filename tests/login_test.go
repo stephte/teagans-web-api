122c801a1f949301ee9751a2f871a9778f1c85fb
stephte <teaganstephenson@gmail.com>
2024-01-19 19:37:58 -0600

    added another login test

--- tests/login_test.go
+++ tests/login_test.go
@@ -40,6 +40,38 @@ func TestValidLogin(t *testing.T) {
 }
 
 
+func TestAnotherValidLogin(t *testing.T) {
+	// first we need to initialize the test db or something, and add it to r as context...
+	// would be cool if we could get this into some kind of before all hook
+	testHelper := testhelper.InitTestDBAndService(t)
+
+	// now init data needed in the DB
+	testHelper.CreateTestAuthUsers()
+	defer testHelper.CleanupAuth()
+
+	reqData, jsonErr := json.Marshal(
+		map[string]interface{}{
+		"email": "Admin@test.com",
+		"password": "testpassword8",
+	})
+	if jsonErr != nil {
+		t.Errorf("expected error to be nil got %v", jsonErr)
+	}
+
+	res := testHelper.SendAsNoOne("post", "/auth/login", reqData)
+	
+	testHelper.AssertStatus(res, 204)
+
+	jwt := res.Header.Get("Authorization")
+	csrf := res.Header.Get("X-CSRF-Token")
+
+	testHelper.Assert(csrf != "", "")
+
+	splitJWT := strings.Split(jwt, ".")
+	testHelper.Assert(len(splitJWT) == 3, "")
+}
+
+
 func TestInvalidLogin(t *testing.T) {
 	testHelper := testhelper.InitTestDBAndService(t)
 	testHelper.CreateTestAuthUsers()
