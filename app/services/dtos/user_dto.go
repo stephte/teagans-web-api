71fe5563f585b738f8bb2fb7e8f2204d26653fe1
stephte <teaganstephenson@gmail.com>
2024-04-20 16:42:27 -0500

    clean up user dto

--- app/services/dtos/user_dto.go
+++ app/services/dtos/user_dto.go
@@ -1,12 +1,5 @@
 package dtos
 
-import (
-	// intrfaceUtils "teagans-web-api/app/utilities/intrface"
-	// "strings"
-	// "errors"
-	// "fmt"
-)
-
 
 type UserInDTO struct {
 	FirstName 	string		`json:"firstName"`
@@ -20,53 +13,7 @@ type UserOutDTO struct {
 	UserInDTO
 }
 
-// How to get password so we can retrieve it, but not send it??
 type CreateUserDTO struct {
 	UserInDTO
 	Password	string
 }
-
-
-// // uses UserDTO to validate the data passed before updating the user with it
-// func ValidateUserMap(data map[string]interface{}) (map[string]interface{}, error) {
-// 	rv := map[string]interface{}{}
-// 	var strct UserInDTO
-
-// 	for key, value := range data {
-// 		capitalKey := strings.Title(key)
-
-// 		// ensure they can't manipulate BaseDTO data
-// 		if strings.Contains(capitalKey, "BaseDTO") {
-// 			return rv, errors.New(fmt.Sprintf("Unpermitted data: %s", key))
-// 		} else if strings.Contains(strings.ToLower(key), "id") {
-// 			continue
-// 		}
-
-// 		typ := intrfaceUtils.GetType(value)
-
-// 		// custom check for role, since it is an enum
-// 		if capitalKey == "Role" {
-// 			if typ == "float64" || typ == "string" {
-// 				rv[capitalKey] = value
-// 				continue
-// 			}
-// 		}
-
-// 		validKey, typErr := intrfaceUtils.CheckKeyValue(strct, capitalKey, typ)
-
-// 		if typErr != nil {
-// 			return rv, typErr
-// 		} else if !validKey {
-// 			return rv, errors.New(fmt.Sprintf("Unpermitted data: %s", key))
-// 		}
-		
-// 		rv[capitalKey] = value
-// 	}
-
-// 	// if interface is empty, return error
-// 	if len(rv) == 0 {
-// 		return rv, errors.New("No valid user data found")
-// 	}
-
-// 	return rv, nil
-// }
