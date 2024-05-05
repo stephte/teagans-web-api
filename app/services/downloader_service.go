6543fa98021a7613cb0c9f30e960a7a9bee8cadd
stephte <teaganstephenson@gmail.com>
2024-05-05 11:23:01 -0500

    update youtube package, add some debugging to find issue

--- app/services/downloader_service.go
+++ app/services/downloader_service.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"errors"
 	"fmt"
+	// "os"
 )
 
 type DownloaderService struct {
@@ -28,24 +29,28 @@ func(this DownloaderService) DownloadVideo(data dtos.YoutubeDataDTO) (dtos.Downl
 
 	qmap := u.Query()
 	var video_id string
-	if strings.Contains(u.Hostname(), "youtube.com") {
-		video_id = qmap.Get("v")
-	} else if strings.Contains(u.Hostname(), "youtu.be") {
+	if strings.Contains(u.Hostname(), "youtu.be") {
 		video_id = strings.Replace(u.Path, "/", "", 1)
 	} else {
-		return dtos.DownloadDTO{}, dtos.CreateErrorDTO(errors.New("must be a youtube url"), 0, false)
+		video_id = qmap.Get("v")
 	}
 
 	if video_id == "" {
-		return dtos.DownloadDTO{}, dtos.CreateErrorDTO(errors.New("Video ID not found"), 0, false)
+		return dtos.DownloadDTO{}, dtos.CreateErrorDTO(errors.New("Video not found"), 0, false)
 	}
 
 	// now get the video data and the stream
 	client := youtube.Client{}
+	fmt.Println("client")
+	fmt.Println(client)
 	video, verr := client.GetVideo(video_id)
 	if verr != nil {
+		fmt.Println("verr")
+		fmt.Println(verr)
 		return dtos.DownloadDTO{}, dtos.CreateErrorDTO(verr, 0, false)
 	}
+	// fmt.Println("video")
+	// fmt.Println(video)
 
 	formats := video.Formats.WithAudioChannels()
 	format := formats[0]
@@ -55,6 +60,12 @@ func(this DownloaderService) DownloadVideo(data dtos.YoutubeDataDTO) (dtos.Downl
 		format = formats[0]
 		audioStr = "_audio"
 	}
+
+	fmt.Println("format.URL")
+	fmt.Println(format.URL)
+
+	// fmt.Println("format")
+	// fmt.Println(format)
 
 	// fmt.Println("Formats:")
 	// for i, format := range formats {
@@ -66,8 +77,16 @@ func(this DownloaderService) DownloadVideo(data dtos.YoutubeDataDTO) (dtos.Downl
 	rv.ContentType = format.MimeType
 	rv.Filereader, rv.Filesize, err = client.GetStream(video, &format)
 	if err != nil {
+		fmt.Println("err")
+		fmt.Println(err)
 		return dtos.DownloadDTO{}, dtos.CreateErrorDTO(err, 0, false)
 	}
+
+	// var byts []byte
+	// rv.Filereader.Read(byts)
+
+	// fmt.Println("rv")
+	// fmt.Println(rv)
 
 	return rv, dtos.ErrorDTO{}
 }
--- go.mod
+++ go.mod
@@ -7,10 +7,10 @@ require (
 	github.com/go-chi/httplog v0.2.5
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
-	github.com/kkdai/youtube/v2 v2.9.0
+	github.com/kkdai/youtube/v2 v2.10.1
 	github.com/microcosm-cc/bluemonday v1.0.26
 	github.com/rs/zerolog v1.28.0
-	golang.org/x/crypto v0.14.0
+	golang.org/x/crypto v0.21.0
 	gorm.io/driver/postgres v1.4.6
 	gorm.io/gorm v1.24.3
 )
@@ -18,10 +18,10 @@ require (
 require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bitly/go-simplejson v0.5.1 // indirect
-	github.com/dlclark/regexp2 v1.10.0 // indirect
-	github.com/dop251/goja v0.0.0-20230828202809-3dbe69dd2b8e // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
-	github.com/google/pprof v0.0.0-20230907193218-d3ddc7976beb // indirect
+	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -30,7 +30,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
